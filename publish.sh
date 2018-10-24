4ee429113088f8a9c7c71481fc61e3f39660d822
Zhang Haifeng <zhanghf@zailingtech.com>
2018-10-24 18:47:20 +0800

    circleci: 修改配置
    Signed-off-by: Zhang Haifeng <zhanghf@zailingtech.com>

--- publish.sh
+++ publish.sh
@@ -54,7 +54,7 @@ find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; d
   chart_name="`basename "$chart"`"
   echo ">>> helm package -u -d $chart_name $chart"
   mkdir -p "$chart_name"
-  helm package -d "$chart_name" "$chart"
+  helm package -u -d "$chart_name" "$chart"
 done
 echo '>>> helm repo index'
 helm repo index .
