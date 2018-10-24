1f0b6d7ed854c7e0d1945c3a4f57ae8d23ba9cde
Zhang Haifeng <zhanghf@zailingtech.com>
2018-10-24 18:54:42 +0800

    circleci: 修改配置，增加自身repo
    Signed-off-by: Zhang Haifeng <zhanghf@zailingtech.com>

--- publish.sh
+++ publish.sh
@@ -4,6 +4,10 @@ set -o pipefail
 
 WORKING_DIRECTORY="$PWD"
 
+[ "$GITHUB_PAGES_USER" ] || {
+  echo "ERROR: Environment variable GITHUB_PAGES_USER is required"
+  exit 1
+}
 [ "$GITHUB_PAGES_REPO" ] || {
   echo "ERROR: Environment variable GITHUB_PAGES_REPO is required"
   exit 1
@@ -20,6 +24,7 @@ WORKING_DIRECTORY="$PWD"
   exit 1
 }
 
+echo "GITHUB_PAGES_USER=$GITHUB_PAGES_USER"
 echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
@@ -40,12 +45,13 @@ chmod +x linux-amd64/helm
 alias helm=/tmp/helm/bin/linux-amd64/helm
 helm version -c
 helm init -c
+helm repo add self https://$GITHUB_PAGES_USER.github.io/$GITHUB_PAGES_REPO
 
-echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
+echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_USER/$GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
+git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_USER/$GITHUB_PAGES_REPO.git" .
 
 echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
@@ -64,7 +70,7 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then
   exit 0
 fi
 
-echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
+echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_USER/$GITHUB_PAGES_REPO"
 git config user.email "$CIRCLE_USERNAME@users.noreply.github.com"
 git config user.name CircleCI
 git add .
